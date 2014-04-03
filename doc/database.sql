b5dab0792f9b4f19965ee94bf1c530193c162927
Andy <andro.tunka@khk.ee>
2014-04-03 16:23:22 +0300

    Update database dump, remove broken links, fix posts support

--- doc/database.sql
+++ doc/database.sql
@@ -1,51 +1,97 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Aprill 03, 2014 kell 03:01 PL
--- Serveri versioon: 5.6.16
--- PHP versioon: 5.5.9
+-- Generation Time: Apr 03, 2014 at 04:10 PM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Andmebaas: `blog`
+-- Database: `blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `post`
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `comment_text` varchar(500) NOT NULL,
+  `comment_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment_author` varchar(20) NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `comment_text`, `comment_created`, `comment_author`) VALUES
+(1, 'Hey hey', '2013-10-16 20:46:10', 'Taunoo'),
+(2, 'Mis toimps?', '2013-10-16 20:47:02', 'Andro'),
+(3, 'Viina on?', '2013-10-16 20:48:19', 'Pets');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
 --
 
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
   `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `post_subject` varchar(225) NOT NULL,
+  `post_subject` varchar(255) NOT NULL,
   `post_text` text NOT NULL,
   `post_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`post_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
--- Andmete tõmmistamine tabelile `post`
+-- Dumping data for table `post`
 --
 
 INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
-(7, 'heyhey', 'hoho', '2014-04-03 11:19:38', 1),
-(8, 'haha', 'hihi', '2014-04-03 11:19:38', 1),
-(9, 'heyhey', 'hoho', '2014-04-03 11:20:01', 1),
-(10, 'haha', 'hihi', '2014-04-03 11:20:01', 1);
+(3, 'Postitus1', 'Siin on esimene postitus', '2014-02-03 14:48:12', 1),
+(5, 'Postitus2', 'See siin on teine postitus', '2014-04-03 14:50:17', 1),
+(6, 'Postitus3', 'See on kolmas postitus mul', '2014-04-03 14:56:29', 1);
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `post_tags`
+-- Table structure for table `post_comments`
+--
+
+DROP TABLE IF EXISTS `post_comments`;
+CREATE TABLE IF NOT EXISTS `post_comments` (
+  `post_id` int(11) unsigned NOT NULL,
+  `comment_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`post_id`,`comment_id`),
+  KEY `comment_id` (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post_comments`
+--
+
+INSERT INTO `post_comments` (`post_id`, `comment_id`) VALUES
+(5, 1),
+(6, 2),
+(6, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_tags`
 --
 
 DROP TABLE IF EXISTS `post_tags`;
@@ -56,10 +102,21 @@ CREATE TABLE IF NOT EXISTS `post_tags` (
   KEY `tag_id` (`tag_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `post_tags`
+--
+
+INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
+(3, 1),
+(5, 2),
+(3, 3),
+(5, 3),
+(6, 3);
+
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `tag`
+-- Table structure for table `tag`
 --
 
 DROP TABLE IF EXISTS `tag`;
@@ -67,12 +124,22 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tag_name` varchar(25) NOT NULL,
   PRIMARY KEY (`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `tag`
+--
+
+INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
+(1, 'taaaaagggg'),
+(2, 'taggitagggiiii'),
+(3, 'tatatatatata'),
+(4, 'teretereterettee');
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `user`
+-- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -85,26 +152,33 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Andmete tõmmistamine tabelile `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
 (1, 'demo', 'demo', 0);
 
 --
--- Tõmmistatud tabelite piirangud
+-- Constraints for dumped tables
 --
 
 --
--- Piirangud tabelile `post`
+-- Constraints for table `post`
 --
 ALTER TABLE `post`
   ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
--- Piirangud tabelile `post_tags`
+-- Constraints for table `post_comments`
+--
+ALTER TABLE `post_comments`
+  ADD CONSTRAINT `post_comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
+  ADD CONSTRAINT `post_comments_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`comment_id`);
+
+--
+-- Constraints for table `post_tags`
 --
 ALTER TABLE `post_tags`
-  ADD CONSTRAINT `post_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`),
-  ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
+  ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
+  ADD CONSTRAINT `post_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
 SET FOREIGN_KEY_CHECKS=1;
--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,7 +1,33 @@
 <div class="span8">
-    <h1><?=$post['andmebaasiväljanimi1']?></h1>
-    <p><?=$post['andmebaasiväljanimi2']?></p>
-    <div>
-        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
-    </div>
+    <h1><?= $post['post_subject'] ?></h1>
 
+    <p><?= $post['post_text'] ?></p>
+
+    <div>
+        <span class="badge badge-success"><?= $post['post_created'] ?></span>
+
+        <div class="pull-right"><span class="label"><?= $post['username'] ?></span>
+            <span class="label">story</span> <span class="label">blog</span>
+            <span class="label">personal</span>
+        </div>
+        <? foreach ($tags as $tag): ?>
+            <a href="<?= BASE_URL ?>tags/view/<?= $tag['tag_id'] ?>"><span class="label label-info"
+                                                                           style="background-color:#5bc0de"><?= $tag['tag_name'] ?></span></a>
+        <? endforeach ?>
+    </div>
+    <hr>
+</div>
+
+
+<? foreach ($comments as $comment): ?>
+    <div class="media">
+        <a class="pull-left" href="#">
+            <img class="media-object" src="<?= ASSETS_URL ?>img/mati.png" alt="...">
+        </a>
+
+        <div class="media-body">
+            <h4 class="media-heading"><?= $post["username"] ?></h4>
+            <?= $comment["comment_text"] ?>
+        </div>
+    </div>
+<? endforeach ?>
